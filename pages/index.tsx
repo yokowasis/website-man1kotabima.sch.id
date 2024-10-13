a1c39fb407b3592eceefb520ad8c579a41385781
Your Name <you@example.com>
2024-10-13 09:02:32 +0800

    somefix

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,25 +14,6 @@ type Props = {
 export default function Index({ allPosts, s }: Props) {
   return (
     <>
-      {/* <Layout>
-        <Head>
-          <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout> */}
       <Head>
         <title>{s.InfoSekolah.Title}</title>
       </Head>
